52429270d3aa764d62c4ee2bf671845605810b8a
Cătălin Frâncu <cata@francu.com>
2008-01-29 22:11:28 +0000

    more work

--- yams.py
+++ yams.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import gtk
+import os
+import os.path
 import pango
 import pygtk
+import random
 
-from yamsData import *
+import yamsData
 
-CONFIG_FILE = '~/.yams'
+CONFIG_FILE = '~/.yams.conf'
 
 MENU = '''<ui>
   <menubar name="MenuBar">
@@ -21,8 +24,48 @@ COLUMN_NAMES = [ '', u'\u21e9 Down \u21e9', u'\u21e9 Free \u21e9',
                  u'\u21e7 Free \u21e7', u'\u21e7 Up \u21e7',
                  'Announced', 'Served' ]
 
-ROW_NAMES = [ 'Ones', 'Twos', 'Threes', 'Fours', 'Fives', 'Sixes',
-              'Max', 'Min', 'Straight', 'Full House', '4 of a Kind', 'Yams' ]
+ANNOUNCED_COLUMN = 5
+SERVED_COLUMN = 6
+
+MSG_ANNOUNCE = 'You announce %s.'
+MSG_FILLED_CELL = 'That cell is already filled.'
+MSG_FIRST_ROLL = 'Click Roll to roll all the dice.'
+MSG_OBEY_ANNOUNCE = 'You announced %s and you must fill it.'
+MSG_SELECT_DICE = 'Select some dice to roll or click a cell to fill it.'
+MSG_RULE_DOWN = u'The \u21e9 Down \u21e9 column must be filled top-to-bottom.'
+MSG_RULE_FDOWN = u'A \u21e9 Free \u21e9 cell can only be filled if the ' +\
+    u'corresponding \u21e9 Down \u21e9 cell was filled.'
+MSG_RULE_FUP = u'A \u21e7 Free \u21e7 cell can only be filled if the ' +\
+    u'corresponding \u21e7 Up \u21e7 cell was filled.'
+MSG_RULE_UP = u'The \u21e7 Up \u21e7 column must be filled bottom-to-top.'
+MSG_THREE_ROLLS = 'You can only roll three times.'
+MSG_TURN = "%s's turn."
+MSG_WRONG_PLAYER = "It is %s's turn, please select the appropriate score sheet."
+
+# Row name, corresponding row on the score sheet
+ROW_PROPS = [ [ 'Ones', 0],
+              [ 'Twos', 1],
+              [ 'Threes', 2],
+              [ 'Fours', 3],
+              [ 'Fives', 4],
+              [ 'Sixes', 5],
+              [ '', -1],
+              [ 'Max', 6],
+              [ 'Min', 7],
+              [ '', -1],
+              [ 'Straight', 8],
+              [ 'Full House', 9],
+              [ '4 of a Kind', 10],
+              [ 'Yams', 11],
+              [ '', -1],
+              [ 'T1', -1],
+              [ 'T2', -1],
+              [ 'T3', -1],
+              [ 'Total', -1] ]
+ROW_T1 = 15
+ROW_T2 = 16
+ROW_T3 = 17
+ROW_TOTAL = 18
 
 DIE_IMAGES = [ 'images/gnome-dice-none.svg',
                'images/gnome-dice-1.svg',
@@ -32,23 +75,169 @@ DIE_IMAGES = [ 'images/gnome-dice-none.svg',
                'images/gnome-dice-5.svg',
                'images/gnome-dice-6.svg' ]
 
+DEFAULT_PLAYERS = [ 'Player 1' ]
+
 class MainWindow:
-    accelGroup = None
-    diceBox = None
+    diceButtons = None
     notebook = None
+    rollButton = None
+    statusBar = None
     window = None
 
-    def doGameNew(self, action):
-        scoreSheets = []
-        for i in range(numPlayers):
-            scoreSheets.append(ScoreSheet())
-        #endfor
+    def loadConfigFile(self):
+        try:
+            f = open(os.path.expanduser(CONFIG_FILE), 'r')
+            for line in f:
+                line = line.strip()
+                parts = line.split('=', 2)
+                if parts[0].strip() == 'playerNames':
+                    names = parts[1].split(',')
+                    yamsData.playerNames = [x.strip() for x in names]
+                    yamsData.numPlayers = len(yamsData.playerNames)
+                #endif
+            #endfor
+            f.close()
+        except IOError:
+            yamsData.numPlayers = len(DEFAULT_PLAYERS)
+            yamsData.playerNames = DEFAULT_PLAYERS
+        #endtry
     #enddef
-    
+
+    def doGameNew(self, action):
+        self.loadConfigFile()
+        yamsData.scoreSheets = []
+        for i in range(yamsData.numPlayers):
+            yamsData.scoreSheets.append(yamsData.ScoreSheet())
+        #endfor
+
+        while self.notebook.get_n_pages() > 0:
+            self.notebook.remove_page(0)
+        #endwhile
+        for i in range(yamsData.numPlayers):
+            tv = self.makeTreeView()
+            tv.position = i
+            self.notebook.append_page(tv, gtk.Label(yamsData.playerNames[i]))
+            tv.show()
+        #endfor
+
+        yamsData.currentPlayer = -1
+        self.advancePlayer()
+    #enddef
+
+    def onRollButtonClick(self, button):
+        anyActive = sum(x.get_active() for x in self.diceButtons)
+        if yamsData.rolled > 0 and not anyActive:
+            self.setStatusBarMessage(MSG_SELECT_DICE)
+            return
+        #endif
+
+        for i in range(5):
+            if yamsData.rolled == 0 or self.diceButtons[i].get_active():
+                yamsData.dice[i] = random.randint(1, 6)
+                self.setDiceImage(i)
+                self.diceButtons[i].set_active(False)
+            #endif
+        #endfor
+
+        if yamsData.rolled == 0:
+            yamsData.firstRollDice = yamsData.dice[:]
+        #endif
+
+        yamsData.rolled += 1
+        self.setRollButtonCount(yamsData.rolled + 1)
+    #enddef
+
+    def onDieClick(self, button):
+        if yamsData.rolled == 0:
+            self.setStatusBarMessage(MSG_FIRST_ROLL)
+            button.set_active(False)
+        elif yamsData.rolled == 3:
+            self.setStatusBarMessage(MSG_THREE_ROLLS)
+            button.set_active(False)
+        elif button.get_active():
+            self.setDiceImage(button.position, 0)
+        else:
+            self.setDiceImage(button.position)
+        #endif
+    #enddef
+
+    def onSheetClick(self, treeView):
+        cursor = treeView.get_cursor()
+        row = cursor[0][0]
+        ssRow = ROW_PROPS[row][1]
+
+        if cursor[1] == None:
+            # First time we are displaying this tree. There was no click.
+            return
+        #endif
+
+        column = cursor[1].position
+        ssColumn = column - 1
+
+        if yamsData.rolled == 0 or column == 0 or ssRow == -1:
+            # Not rolled, clicked on a column header or clicked in an empty row
+            return
+        #endif
+
+        if treeView.position != yamsData.currentPlayer:
+            # Clicked in the wrong score sheet
+            name = yamsData.playerNames[yamsData.currentPlayer]
+            self.setStatusBarMessage(MSG_WRONG_PLAYER % name)
+            return
+        #endif
+
+        if yamsData.isFilled(ssRow, ssColumn):
+            # Clicked on a cell that's already filled
+            self.setStatusBarMessage(MSG_FILLED_CELL)
+            return
+        #endif
+
+        if not yamsData.canBeFilled(ssRow, ssColumn):
+            # Clicked on a cell that cannot yet be filled
+            if column == 1:
+                self.setStatusBarMessage(MSG_RULE_DOWN)
+            elif column == 2:
+                self.setStatusBarMessage(MSG_RULE_FDOWN)
+            elif column == 3:
+                self.setStatusBarMessage(MSG_RULE_FUP)
+            elif column == 4:
+                self.setStatusBarMessage(MSG_RULE_UP)
+            #endif
+            return
+        #endif
+            
+        if yamsData.rolled == 1 and column == ANNOUNCED_COLUMN \
+                and yamsData.announced == None:
+            # Announce something
+            self.setStatusBarMessage(MSG_ANNOUNCE % ROW_PROPS[row][0], False)
+            yamsData.announced = row
+            #endif
+            return
+        #endif
+
+        if yamsData.announced != None and \
+                (column != ANNOUNCED_COLUMN or row != yamsData.announced):
+            self.setStatusBarMessage(MSG_OBEY_ANNOUNCE %
+                                     ROW_PROPS[yamsData.announced][0])
+            return
+        #endif
+
+        value = yamsData.writeScore(ssRow, ssColumn)
+        model = treeView.get_model()
+        model.set(model.get_iter(row), column, value if value else u'\u2015')
+
+        ssCol = yamsData.scoreSheets[yamsData.currentPlayer].columns[ssColumn]
+        model.set(model.get_iter(ROW_T1), column, ssCol.getT1())
+        model.set(model.get_iter(ROW_T2), column, ssCol.getT2())
+        model.set(model.get_iter(ROW_T3), column, ssCol.getT3())
+        model.set(model.get_iter(ROW_TOTAL), column, ssCol.getTotal())        
+        self.advancePlayer()
+    #enddef
+
     def makeMenu(self):
         uiManager = gtk.UIManager()
-        self.accelGroup = uiManager.get_accel_group()
-        self.window.add_accel_group(self.accelGroup)
+        accelGroup = uiManager.get_accel_group()
+        self.window.add_accel_group(accelGroup)
         actionGroup = gtk.ActionGroup('UIManager')
         actionGroup.add_actions(
             [('Game', None, '_Game'),
@@ -64,12 +253,12 @@ class MainWindow:
     def makeTreeView(self):
         liststore = gtk.ListStore(str, str, str, str, str, str, str)
 
-        for row in range(12):
-            liststore.append([ROW_NAMES[row]] + [''] * 6)
+        for (rowName, ignored) in ROW_PROPS:
+            liststore.append([rowName] + [''] * 6)
         #endfor
 
         # create the TreeView using liststore
-        treeview = gtk.TreeView(liststore)
+        treeView = gtk.TreeView(liststore)
 
         # create a CellRendererText to render the data
         cellHeaderCol = gtk.CellRendererText()
@@ -78,8 +267,8 @@ class MainWindow:
 
         # create the TreeViewColumns to display the data
         for i in range(7):
-            tvcolumn = gtk.TreeViewColumn(COLUMN_NAMES[i])
-            treeview.append_column(tvcolumn)
+            tvColumn = gtk.TreeViewColumn(COLUMN_NAMES[i])
+            treeView.append_column(tvColumn)
 
             if i == 0:
                 cell = cellHeaderCol
@@ -87,41 +276,101 @@ class MainWindow:
                 cell = cellNormalCol
             #endif
 
-            tvcolumn.pack_start(cell, True)
-            tvcolumn.add_attribute(cell, 'text', i)
-            tvcolumn.set_min_width(100)
+            tvColumn.pack_start(cell, True)
+            tvColumn.add_attribute(cell, 'text', i)
+            tvColumn.set_min_width(100)
+            tvColumn.position = i
         #endfor
 
-        liststore.set(liststore.get_iter(2), 4, "qqqqq")
-        treeview.get_selection().set_mode(gtk.SELECTION_NONE)
-        treeview.set_rules_hint(True)
-        #treeview.set_grid_lines(gtk.TREE_VIEW_GRID_LINES_VERTICAL)
-        return treeview
+        treeView.get_selection().set_mode(gtk.SELECTION_NONE)
+        treeView.set_rules_hint(True)
+        treeView.set_grid_lines(gtk.TREE_VIEW_GRID_LINES_VERTICAL)
+        treeView.connect('cursor-changed', self.onSheetClick)
+        return treeView
     #enddef
 
     def makeDiceBox(self):
         box = gtk.VBox(False, 2)
-        self.diceImages = []
+
+        toolbar = gtk.Toolbar()
+        toolbar.set_orientation(gtk.ORIENTATION_VERTICAL)
+        toolbar.set_style(gtk.TOOLBAR_ICONS)
+        toolbar.set_show_arrow(False)
+        box.pack_start(toolbar, False)
+
+        self.diceButtons = []
 
         for i in range(5):
             img = gtk.Image()
-            self.diceImages += [img]
-            box.pack_start(img, False)
+            button = gtk.ToggleToolButton()
+            button.set_icon_widget(img)
+            button.position = i
+            button.connect('clicked', self.onDieClick)
+            self.diceButtons += [button]
+            toolbar.insert(button, i)
             self.setDiceImage(i, 0)
         #endfor
 
+        self.rollButton = gtk.Button('')
+        self.setRollButtonCount(1)
+        self.rollButton.connect('clicked', self.onRollButtonClick)
+        box.pack_end(self.rollButton, False)
 
         box.show()
         return box
     #endif
 
-    def setDiceImage(self, count, value):
-        self.diceImages[count].set_from_file(DIE_IMAGES[value])
+    def setDiceImage(self, count, value = None):
+        if value == None:
+            value = yamsData.dice[count]
+        #endif
+        image = self.diceButtons[count].get_icon_widget()
+        image.set_from_file(DIE_IMAGES[value])
+    #enddef
+
+    def setRollButtonCount(self, count):
+        if count > 3:
+            self.rollButton.set_label('Roll 3/3')
+            self.rollButton.set_sensitive(False)
+        else:
+            self.rollButton.set_label('Roll %d/3' % count)
+            self.rollButton.set_sensitive(True)
+        #endif
+    #enddef
+
+    def setStatusBarMessage(self, msg, bell = True):
+        contextId = self.statusBar.get_context_id('unique')
+        # Never stack messages up
+        self.statusBar.pop(contextId)
+        self.statusBar.push(contextId, msg)
+        if bell:
+            self.statusBar.error_bell()
+        #endif
+    #enddef
+
+    def advancePlayer(self):
+        for i in range(5):
+            yamsData.dice[i] = 0
+            self.setDiceImage(i)
+            self.diceButtons[i].set_active(False)
+        #endfor
+
+        yamsData.rolled = 0
+        self.setRollButtonCount(1)
+        yamsData.announced = None
+        yamsData.currentPlayer += 1
+        if yamsData.currentPlayer == yamsData.numPlayers:
+            yamsData.currentPlayer = 0
+        #endif
+        self.notebook.set_current_page(yamsData.currentPlayer)
+
+        name = yamsData.playerNames[yamsData.currentPlayer]
+        self.setStatusBarMessage(MSG_TURN % name, False)
     #enddef
 
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_title("Yams")
+        self.window.set_title('Yams')
         self.window.connect('destroy', lambda w: gtk.main_quit())
 
         vbox = gtk.VBox(False, 2)
@@ -141,17 +390,16 @@ class MainWindow:
         hbox.pack_start(self.notebook, False);
         self.notebook.show()
 
-        for i in range(3):
-            tv = self.makeTreeView()
-            self.notebook.append_page(tv, gtk.Label("Player %d" % (i + 1)))
-            tv.show()
-        #endfor
+        self.statusBar = gtk.Statusbar()
+        vbox.pack_start(self.statusBar, False)
+        self.statusBar.show()
 
         self.window.show_all()
     #enddef
 #endclass
 
 if __name__ == "__main__":
-    MainWindow()
+    window = MainWindow()
+    window.doGameNew(None)
     gtk.main()
 #endif
