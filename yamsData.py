1c1bc40ce4e1b5e9c1a4a0229b85a0516cc7a2bc
cata <cata@edcfc504-96a5-478d-8c4c-1aafda250408>
2008-01-29 22:11:28 +0000

    more work

--- yamsData.py
+++ yamsData.py
@@ -2,15 +2,25 @@
 
 from yamsDieLogic import *
 
-numPlayers = 1
+announced = None
+currentPlayer = 0
+dice = [0, 0, 0, 0, 0]
+firstRollDice = None
+numPlayers = None
+playerNames = None
+rolled = 0
 scoreSheets = None
 
 COLUMN_NORMAL = 0
-COLUMN_CALLED = 1
+COLUMN_ANNOUNCED = 1
 COLUMN_SERVED = 2
 
 UPPER_HALF_BONUS = 30
 UPPER_HALF_LIMIT = 60
+LOWER_HALF_BONUS1 = 50
+LOWER_HALF_LIMIT1 = 150
+LOWER_HALF_BONUS2 = 100
+LOWER_HALF_LIMIT2 = 300
 
 STRAIGHT_SCORES = [0, 15, 20]
 FULL_SCORES = [0, 25, 30, 35, 40, 45, 50]
@@ -23,13 +33,16 @@ class Column:
     type = None
 
     def __init__(self, type):
-        #self.cells = [0] * 12
-        self.cells = [3, 6, 9, 12, 15, 17, 9, 10, 0, 15, 1, 3]
+        self.cells = [0] * 12
         self.filled = [False] * 12
         self.type = type
     #enddef
 
     def getT1(self):
+        if sum(self.filled[0:6]) == 0:
+            return None
+        #endif
+
         t1 = sum(self.cells[0:6])
         if t1 >= UPPER_HALF_LIMIT:
             if self.type == COLUMN_NORMAL:
@@ -42,22 +55,30 @@ class Column:
 
     def getT2(self):
         if self.filled[0] and self.filled[6] and self.filled[7]:
+            if self.cells[6] == 0 or self.cells[7] == 0:
+                # Rare case: one of the cells was cut, e.g. in the served column
+                return 0
+            #endif
             return max(0, (self.cells[6] - self.cells[7]) * self.cells[0])
         else:
-            return 0
+            return None
         #endif
     #enddef
 
     def getT3(self):
-        s = sum(self.cells[8:])
-        if self.type == COLUMN_CALLED:
-            return s  # No T3 bonuses on the called column
+        if sum(self.filled[8:]) == 0:
+            return None
         #endif
 
-        if s >= 300:
-            bonus = 100
-        elif s >= 150:
-            bonus = 50
+        s = sum(self.cells[8:])
+        if self.type == COLUMN_ANNOUNCED:
+            return s  # No T3 bonuses on the announced column
+        #endif
+
+        if s >= LOWER_HALF_LIMIT2:
+            bonus = LOWER_HALF_BONUS2
+        elif s >= LOWER_HALF_LIMIT1:
+            bonus = LOWER_HALF_BONUS1
         else:
             bonus = 0
         #endif
@@ -70,21 +91,38 @@ class Column:
     #enddef
 
     def getTotal(self):
-        return self.getT1() + self.getT2() + self.getT3()
+        t1 = self.getT1()
+        t2 = self.getT2()
+        t3 = self.getT3()
+        if t1 == None and t2 == None and t3 == None:
+            return None
+        else:
+            return (t1 if t1 else 0) + (t2 if t2 else 0) + (t3 if t3 else 0)
+        #endif
     #enddef
 
     def setCombo(self, pos, dice):
         # TODO: HANDLE THE ANNOUNCED COLUMN!
         self.filled[pos] = True;
 
+        if self.type == COLUMN_SERVED and rolled > 1:
+            # Equivalent to a cut.
+            self.cells[pos] = 0
+            return
+        #endif
+
         # Set the field's value according to the dice combo
-        if pos <= 5:
-            sum = dice.count(pos + 1) * (pos + 1)
+        if pos < 6:
+            origCount = firstRollDice.count(pos + 1)
+            count = dice.count(pos + 1)
+            digitSum = count * (pos + 1)
             if pos == 0 and self.type == COLUMN_SERVED:
                 # Double points for served aces
-                self.cells[pos] = 2 * sum
+                self.cells[pos] = 2 * digitSum
+            elif self.type == COLUMN_ANNOUNCED:
+                self.cells[pos] = digitSum * (2 if count > origCount else 1)
             else:
-                self.cells[pos] = sum
+                self.cells[pos] = digitSum
         elif pos <= 7:
             self.cells[pos] = sum(dice)
         elif pos == 8:
@@ -95,6 +133,11 @@ class Column:
             self.cells[pos] = FK_SCORES[dl_get4KType(dice)]
         elif pos == 11:
             self.cells[pos] = YAMS_SCORES[dl_getYamsType(dice)]
+        #endif
+
+        # Announced figures are doubled unless they are served.
+        if pos >= 8 and self.type == COLUMN_ANNOUNCED and rolled > 1:
+            self.cells[pos] *= 2
         #endif
     #enddef
 #endclass
@@ -107,9 +150,35 @@ class ScoreSheet:
         for i in range(4):
             self.columns.append(Column(COLUMN_NORMAL))
         #endfor
-        self.columns.append(Column(COLUMN_CALLED))
+        self.columns.append(Column(COLUMN_ANNOUNCED))
         self.columns.append(Column(COLUMN_SERVED))
-        self.columns[0].setCombo(11, [4, 4, 4, 4, 4])
-        print self.columns[0].cells
     #enddef
 #endclass
+
+def writeScore(row, column):
+    ssCol = scoreSheets[currentPlayer].columns[column]
+    ssCol.setCombo(row, dice)
+    return ssCol.cells[row]
+#enddef
+
+def isFilled(row, column):
+    return scoreSheets[currentPlayer].columns[column].filled[row]
+#enddef
+
+def canBeFilled(row, column):
+    if column == 0:
+        ssCol = scoreSheets[currentPlayer].columns[0]
+        return row == 0 or ssCol.filled[row - 1]
+    elif column == 1:
+        ssCol = scoreSheets[currentPlayer].columns[0]
+        return ssCol.filled[row]
+    elif column == 2:
+        ssCol = scoreSheets[currentPlayer].columns[3]
+        return ssCol.filled[row]
+    elif column == 3:
+        ssCol = scoreSheets[currentPlayer].columns[3]
+        return row == 11 or ssCol.filled[row + 1]
+    else:
+        return True
+    #endif
+#enddef
