3243f13d52fd1ae08e4453b69e90bab95e859cdf
cata <cata@edcfc504-96a5-478d-8c4c-1aafda250408>
2008-01-31 22:54:34 +0000

    manual, license, about screen...

--- yamsData.py
+++ yamsData.py
@@ -1,4 +1,19 @@
-# Yams global variables and data types
+# Copyright 2008 Catalin Francu <cata@francu.com>
+#
+# This file is part of Yams.
+#
+# Yams is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Yams is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Yams.  If not, see <http://www.gnu.org/licenses/>.
 
 from yamsDieLogic import *
 
--- yamsDieLogic.py
+++ yamsDieLogic.py
@@ -1,4 +1,19 @@
-# Yams die manipulation and counting
+# Copyright 2008 Catalin Francu <cata@francu.com>
+#
+# This file is part of Yams.
+#
+# Yams is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Yams is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Yams.  If not, see <http://www.gnu.org/licenses/>.
 
 # Returns 1 for a low straight, 2 for a high straight, or 0 for no straight
 def dl_getStraightType(dice):
@@ -30,8 +45,7 @@ def dl_get4KType(dice):
     dice.sort()
     # Four of a kinds look like A A A A B or A B B B B.
     # The die in the middle dictates the 4K type
-    if dice[1] == dice[2] and dice[2] == dice[3] and \
-            (dice[0] == dice[1] or dice[3] == dice[4]):
+    if (dice[1] == dice[3]) and (dice[0] == dice[1] or dice[3] == dice[4]):
         return dice[2]
     else:
         return 0
@@ -40,8 +54,8 @@ def dl_get4KType(dice):
 
 # Returns 1-6 for the yams type or 0 for no yams
 def dl_getYamsType(dice):
-    if dice[0] == dice[1] and dice[1] == dice[2] and \
-            dice[2] == dice[3] and dice[3] == dice[4]:
+    dice.sort()
+    if dice[0] == dice[4]:
         return dice[0]
     else:
         return 0
